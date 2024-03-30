a66243f71bc98f1d396ff2a1be0dd3867db0e1db
Your Name <you@example.com>
2024-03-30 20:21:30 +0500

    push my code on github

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "enter a frist number", type: "number", name: "firstNumber" },
+  { message: "enter a second number", type: "number", name: "secondNumber" },
+  {
+    message: "select  one of the opreators to perfom opreation",
+    type: "list",
+    name: "opreator",
+    choices: ["Addition", "subtraction", "Multiplication", "Division"],
+  },
+]);
+console.log(answer)
+if (answer.opreator==="Addition") {
+  console.log(answer.firstNumber+answer.secondNumber);
+}
+else if (answer.opreator==="subtraction") {
+  console.log(answer.firstNumber-answer.secondNumber);
+}
+if (answer.opreator==="Multiplication") {
+  console.log(answer.firstNumber*answer.secondNumber);
+}
+if (answer.opreator==="Division") {
+  console.log(answer.firstNumber/answer.secondNumber);
+}
